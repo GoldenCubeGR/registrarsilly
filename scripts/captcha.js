9e5ff7bad95e8531ad6558be016b43985c382e82
goldenboys2011 <ody.chryssos@gmail.com>
2024-07-10 15:25:06 +0300

    Update captcha.js\

--- scripts/captcha.js
+++ scripts/captcha.js
@@ -1,29 +1,3 @@
-function onLoadCallback() {
-    grecaptcha.render('captcha', {
-        sitekey: "6LdKSPYpAAAAAMhVOwGhAitTpIvWIdTaLBTurft9",
-        callback: successCallback,
-    });
-}
-
-function successCallback(token) {
-    // Send the token to the server
-    fetch('https://varying-charity-3feds-registrar-ecab180f.koyeb.app//verify-recaptcha', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: `g-recaptcha-response=${token}`
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.success) {
-            alert('Verification successful');
-            document.getElementById('regBtnMod').disabled = false;
-        } else {
-            
-        }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+function successCallback() {
+    document.getElementById('regBtnMod').disabled = false;
 }
